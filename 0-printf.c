f5c46a73c6aaa686f10974e8813b379221397b0f
Nathanage3 <sisay954@gmail.com>
2023-06-25 05:27:06 -0700

    The mostly used format specifiers %c and %s

---
+++ 0-printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _putchar - it works like std putchar
+ * @c: the ascii value of chars
+ * Return: the ascii value of c
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - operates as std printf
+ *  @format: the koind of format specifiers passed to this function
+ *  Return: The index count
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int k = 0;
+	char l;
+	char *w;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				_putchar('%');
+			}
+			else
+			{
+				switch (*format)
+				{
+					case 'c':
+						l = (char)va_arg(args, int);
+						_putchar(l);
+						k++;
+						break;
+					case 's':
+						w = va_arg(args, char *);
+						while (*w)
+						{
+							_putchar(*w);
+							w++;
+							k++;
+						}
+						break;
+					default:
+						_putchar(format[-1]);
+						_putchar(*format);
+						k += 2;
+						break;
+				}
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			k++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (k);
+}
