a59897196998496ae6ce60261a2db72dc028d26c
Tekalign Amera <106791681+tekalign11@users.noreply.github.com>
2023-06-25 21:23:24 +0300

    handling format specifiers for modulo char and string

--- 0-printf.c
+++ 0-printf.c
@@ -1,26 +1,14 @@
 #include "main.h"
 /**
- * _putchar - it works like std putchar
- * @c: the ascii value of chars
- * Return: the ascii value of c
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * _printf - operates as std printf
  *  @format: the koind of format specifiers passed to this function
  *  Return: The index count
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int k = 0;
-	char l;
-	char *w;
+	char l, *w;
 
 	va_start(args, format);
 	while (*format)
