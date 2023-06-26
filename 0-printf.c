211afb9297355347f23bbdbf0730bc9c07f5fcc6
Nathanage3 <sisay954@gmail.com>
2023-06-26 07:20:31 -0700

    now

--- 0-printf.c
+++ 0-printf.c
@@ -1,35 +1,5 @@
 #include "main.h"
 /**
- * num_to_string - number to string convertor
- * @_num: number
- * @str: string
- */
-void num_to_string(int _num, char *str)
-{
-	int i = 0, is_minus = 0, _digit, temp = _num, _divisor = 1;
-
-	if (_num == 0)
-	{str[0] = '0';
-		str[1] = '\0';
-		return; }
-	if (_num < 0)
-	{is_minus = 1;
-		_num = -(_num); }
-	if (is_minus)
-	{str[i] = '-';
-		i++; }
-	while ((temp / 10) != 0)
-	{_divisor *= 10;
-		temp /= 10; }
-	while (_divisor != 0)
-	{_digit = _num / _divisor;
-		str[i] = '0' + _digit;
-		i++;
-		_num %= _divisor;
-		_divisor /= 10; }
-	str[i] = '\0';
-}
-/**
  * _printf - operates as std printf
  *  @format: the kind of format specifiers passed to this function
  *  Return: The index count
@@ -37,8 +7,8 @@ void num_to_string(int _num, char *str)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int k = 0, num, i = 0;
-	char l, num_char[12], *w;
+	int k = 0;
+	char l, *w;
 
 	va_start(args, format);
 	while (*format)
@@ -65,16 +35,6 @@ int _printf(const char *format, ...)
 					w++;
 					k++; }
 			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				num = va_arg(args, int);
-				num_to_string(num, num_char);
-				while (num_char[i] != '\0')
-				{_putchar(num_char[i]);
-					i++;
-					k++; }
-			}
-
 			else
 			{_putchar(format[-1]);
 				_putchar(*format);
@@ -83,8 +43,7 @@ int _printf(const char *format, ...)
 		else
 		{_putchar(*format);
 			k++; }
-		format++;
-	}
+		format++; }
 	va_end(args);
 	return (k);
 }
