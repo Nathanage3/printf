db100def9a62ce794e75bda8cfb99cde367ac4f2
Nathanage3 <sisay954@gmail.com>
2023-06-26 06:39:03 -0700

    format specifiers

--- 0-printf.c
+++ 0-printf.c
@@ -1,69 +1,88 @@
 #include "main.h"
 /**
- * _putchar - putchar
- * @c: char c
- * Return: c
+ * num_to_string - number to string convertor
+ * @_num: number
+ * @str: string
  */
-int _putchar(char c)
+void num_to_string(int _num, char *str)
 {
-	return (write(1, &c, 1));
+	int i = 0, is_minus = 0, _digit, temp = _num, _divisor = 1;
+
+	if (_num == 0)
+	{str[0] = '0';
+		str[1] = '\0';
+		return; }
+	if (_num < 0)
+	{is_minus = 1;
+		_num = -(_num); }
+	if (is_minus)
+	{str[i] = '-';
+		i++; }
+	while ((temp / 10) != 0)
+	{_divisor *= 10;
+		temp /= 10; }
+	while (_divisor != 0)
+	{_digit = _num / _divisor;
+		str[i] = '0' + _digit;
+		i++;
+		_num %= _divisor;
+		_divisor /= 10; }
+	str[i] = '\0';
 }
 /**
  * _printf - operates as std printf
- *  @format: the koind of format specifiers passed to this function
+ *  @format: the kind of format specifiers passed to this function
  *  Return: The index count
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int k = 0;
-	char l, *w;
+	int k = 0, num, i = 0;
+	char l, num_char[12], *w;
 
 	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
-		{
-			format++;
+		{format++;
 			if (*format == '\0')
 				break;
 			else if (*format == '%')
-			{
-				_putchar('%');
+			{_putchar('%');
 				k++; }
-			else
-			{
-				switch (*format)
+			else if (*format == 'c')
+			{l = (char)va_arg(args, int);
+				_putchar(l);
+				k++;
+				break; }
+			else if (*format == 's')
+			{w = va_arg(args, char *);
+				while (*w != '\0')
 				{
-					case 'c':
-						l = (char)va_arg(args, int);
-						_putchar(l);
-						k++;
+					if (*w == '\0')
 						break;
-					case 's':
-						w = va_arg(args, char *);
-						while (*w != '\0')
-						{
-							if (*w == '\0')
-								break;
-							_putchar(*w);
-							w++;
-							k++;
-						}
-						break;
-					default:
-						_putchar(format[-1]);
-						_putchar(*format);
-						k += 2;
-						break;
-				}
+					_putchar(*w);
+					w++;
+					k++; }
 			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				num = va_arg(args, int);
+				num_to_string(num, num_char);
+				while (num_char[i] != '\0')
+				{_putchar(num_char[i]);
+					i++;
+					k++; }
+			}
+
+			else
+			{_putchar(format[-1]);
+				_putchar(*format);
+				k += 2; }
 		}
 		else
-		{
-			_putchar(*format);
-			k++;
-		}
+		{_putchar(*format);
+			k++; }
 		format++;
 	}
 	va_end(args);
--- main.h
+++ main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
