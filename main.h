1bfb7f7befca4615f8cff1e5665daf6abd9a1878
Nathanage3 <sisay954@gmail.com>
2023-06-26 07:07:29 -0700

    many trials

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+
+void num_to_string(int _num, char *str);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
