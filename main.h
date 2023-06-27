3f57fdc6ec4ee2179d0db2ee4aae90bddfe51020
Tekalign Amera <106791681+tekalign11@users.noreply.github.com>
2023-06-27 22:46:33 +0300

    Update main.h

--- main.h
+++ main.h
@@ -16,5 +16,7 @@ int _printf_percent(va_list args);
 int _printf(const char *format, ...);
 int _print_b(va_list binary);
 int _print_u(va_list unsign);
+int _print_x(va_list *shexa);
+int _print_X(va_list lhexa);
 
 #endif
