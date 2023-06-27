d2d95da05c1c0c6159be7e9fc2bf38ec308c51bf
Nathanage3 <sisay954@gmail.com>
2023-06-27 12:46:55 -0700

    Save local changes to main

--- main.h
+++ main.h
@@ -10,11 +10,9 @@
 
 int _printf_s(va_list string);
 int _str_length_s(char *string);
-void _puts(const char *string);
 int _putchar(char c);
 int _printf_c(va_list ch);
 int _printf_percent(va_list args);
 int _printf(const char *format, ...);
-void change_to_b(int pdec);
 
 #endif
